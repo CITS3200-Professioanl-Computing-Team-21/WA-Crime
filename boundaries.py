b8ced0285386f957e103d3ede61f20d1901779cd
aditya-gupta21 <aadigupta853@gmail.com>
2021-10-18 15:30:42 +0800

    Cleaning up code

--- boundaries.py
+++ boundaries.py
@@ -12,11 +12,26 @@ from shapely.ops import unary_union
 from geopy.geocoders import Nominatim
 from geojson import Point, Feature, FeatureCollection, dump
 
-# constant variables conatining paths to data files
+# directory path
+PATH = os.getcwd() 
+
+# constant variables containing main data file names
 CRIME = 'crime.csv'
 LANDGATE = 'Localities_LGATE_234_WA_GDA2020_Public.geojson'
 ZONES = 'Suburb Locality.csv'
 FILES = [CRIME, LANDGATE, ZONES]
+
+# constant variables containing centroid file names
+COORDINATES_C = PATH + '/centroids/coordinates.json'
+STATIONS_C = PATH + '/centroids/station_centroids.json'
+DISTRICTS_C = PATH + '/centroids/district_centroids.json'
+REGIONS_C = PATH + '/centroids/region_centroids.json'
+
+# constant variables containing geojson file names
+SUBURBS_G = PATH + '/zones/suburbs.geojson'
+STATIONS_G = PATH + '/zones/stations.geojson'
+DISTRICTS_G = PATH + '/zones/districts.geojson'
+REGIONS_G = PATH + '/zones/regions.geojson'
 
 # function - to check if required files exist
 def check_file(file):
@@ -44,34 +59,39 @@ def data_loading(crime, landgate, zones):
         zones[i] = np.unique(np.array(zones[i]))
     return (landgate_data, landgate_locations, crime_locations, zones_data, zones)
 
+# function - to determine locations that may not be present across all data files
 def check_naming(landgate_locations, crime_locations, zones):
     for location in crime_locations:
+        # locations in CRIME that are not present in ZONES
         missing1 = [location for location in crime_locations if location not in zones['suburbs']]
+        # locations in CRIME that are not present in LANDGATE
         missing2 = [location for location in crime_locations if location not in landgate_locations]
     changes = []
     if not missing1:
         if missing2: 
             for location in missing2:
-                highest = process.extractOne(location, landgate_locations)
+                # matches missing locations which are mismatched but have very similar spelling
+                highest = process.extractOne(location, landgate_locations) # only extracts the matched location with the highest similarity
                 landgate_location, score = highest[0], highest[1]
-                if landgate_location not in crime_locations and score > 85:
+                if landgate_location not in crime_locations and score > 85: # only locations with a similarity score of >85 are chosen
                     changes.append((location, landgate_location))
                     missing2.remove(location)
     return (missing2, changes)
 
+# function - to determine whether the matched missing locations are feasible
 def missing_locations(zones_data, zones, missing2):
     station_coordinates = {}
+    # loop to determine the station coordinates of each station
     for station in zones['stations']:
-        # calling the Nominatim tool
+        # calling the Nominatim tool from the Geopy API
         loc = Nominatim(user_agent="GetLoc") 
-        # entering the location name
+        # entering the location name and obtaining coordinates from API
         getLoc = loc.geocode(station+', WA, Australia')
         station_coordinates[station] = (getLoc.latitude, getLoc.longitude)
     missing_landgate = {}
+    # loop to determine the coordinates of each missing location and to check whether it falls within a 200km radius of its designated station
     for location in missing2:
-        # calling the Nominatim tool
         loc = Nominatim(user_agent="GetLoc") 
-        # entering the location name
         getLoc = loc.geocode(location+', WA, Australia')
         if getLoc:
             missing_point = (getLoc.latitude, getLoc.longitude)
@@ -83,6 +103,7 @@ def missing_locations(zones_data, zones, missing2):
         else : print('not found', location)
     return (missing_landgate)
 
+# function - for the matched locations that are deemed feasible, that location is classfied into a boundary from LANDGATE boundaries
 def assign_missing_boundaries(missing_landgate, landgate_data, changes, crime_locations):
     for location in missing_landgate:
         point = Point(missing_landgate[location][1], missing_landgate[location][0])
@@ -93,6 +114,7 @@ def assign_missing_boundaries(missing_landgate, landgate_data, changes, crime_lo
                 changes.append((location, landgate_location))
     return (changes)
 
+# function - to generate the final zoning and centroid files required for plotting
 def zoning_boundaries(changes, landgate_data, zones, crime_locations, landgate_locations, zones_data):
     final_data = {'suburbs': {}, 'stations': {}, 'districts': {}, 'regions': {}}
     for zone_type in zones:
@@ -122,15 +144,15 @@ def zoning_boundaries(changes, landgate_data, zones, crime_locations, landgate_l
                 polygons = []
                 for location in final_data[zone_type][place]:
                     polygons.append(shape(final_data['suburbs'][location]))
-    #             boundary = gpd.GeoSeries(unary_union(polygons).simplify(tolerance=0.001))
+                # combining all of the boundaries in the specific zone type to form one boundary
                 polygon = unary_union(polygons).simplify(tolerance=0.001)
+                # obtaining lat, lng of the centre point of the generated boundaries
                 try:
                     x = polygon.centroid.x
                     y = polygon.centroid.y
                 except IndexError:
                     x = ''
                     y = ''
-    #             polygon = MultiPolygon(boundary)
                 if zone_type == 'stations':
                     features_stations.append(Feature(geometry=polygon, properties={'zone_type': zone_type, 'name': place}))
                     stations_dict[place] = [y, x]
@@ -144,7 +166,7 @@ def zoning_boundaries(changes, landgate_data, zones, crime_locations, landgate_l
 
     landgate_gpd = gpd.read_file(LANDGATE)
     selected_features = landgate_gpd[['name', 'postcode', 'land_area','geometry']]
-    selected_features.to_file('/Users/adityagupta/Documents/GitHub/WA-Crime/zones/suburbs.geojson', driver="GeoJSON")
+    selected_features.to_file(PATH + '/zones/suburbs.geojson', driver="GeoJSON")
 
     feature_collection_stations = FeatureCollection(features_stations)
     with open('stations.geojson', 'w') as f:
@@ -173,58 +195,55 @@ def zoning_boundaries(changes, landgate_data, zones, crime_locations, landgate_l
 
     return(final_data)
 
+# function - to generate the heat map and plot anomalies
 def choropleth(query):
+    # check for new line character
     if query[0][-1] == '\n':
         query[0] = query[0][:-1]
-    #results, stat, png = mp.main(query)
-    path = os.getcwd()
-    COORDINATES = path + "/centroids/coordinates.json"
-    STATIONS = path + "/centroids/station_centroids.json"
-    DISTRICTS = path + "/centroids/district_centroids.json"
-    REGIONS = path + "/centroids/region_centroids.json"
 
-    with open(r'{}'.format(COORDINATES)) as f:
+    # loading centre points (centroids) of all zone types
+    with open(r'{}'.format(COORDINATES_C)) as f:
         coordinates_suburbs = json.load(f)
-    with open(r'{}'.format(STATIONS)) as f:
+    with open(r'{}'.format(STATIONS_C)) as f:
         coordinates_stations = json.load(f)
-    with open(r'{}'.format(DISTRICTS)) as f:
+    with open(r'{}'.format(DISTRICTS_C)) as f:
         coordinates_districts = json.load(f)
-    with open(r'{}'.format(REGIONS)) as f:
+    with open(r'{}'.format(REGIONS_C)) as f:
         coordinates_regions = json.load(f)
 
+    # obtaining query results from filter.py
     results, anomalies, plot = filter.filter(query[0], query[1], query[2], query[3], query[4])
     results['name'] = results['name'].str.upper()
+    # adding another column for log transformed data
     results['log'] = np.log(results['sum']+1)
 
-    #C:\Users\User\OneDrive\Uni\CITS3200\WA-Crime\zones
-
     if (query[1] == 'suburb') or (query[1] == 'station' and query[0].lower() != 'all'):
-        data = gpd.read_file(path + '/zones/suburbs.geojson')
+        data = gpd.read_file(SUBURBS_G)
         starting_zoom = 12
         fields = ['name', 'postcode', 'land_area','sum']
         aliases = ['Name', 'Postcode', 'Land Area', query[-1].upper()+' Crime Frequency']
     if (query[1] == 'district' and query[0].lower() != 'all') or (query[1] == 'station' and query[0].lower() == 'all'):
-        if query[1] == 'station' and query[0].lower() == 'all':
-            starting_zoom = 5
-        else: starting_zoom = 9
-        data = gpd.read_file(path + '/zones/stations.geojson')
+        data = gpd.read_file(STATIONS_G)
+        starting_zoom = 9
         fields = ['name', 'zone_type', 'sum']
         aliases = ['Name', 'Zone Type', query[-1].upper()+' Crime Frequency']
     if (query[1] == 'region' and query[0].lower() != 'all') or (query[1] == 'district' and query[0].lower() == 'all'):
-        data = gpd.read_file(path + '/zones/districts.geojson')
+        data = gpd.read_file(DISTRICTS_G)
         starting_zoom = 5
         fields = ['name', 'zone_type', 'sum']
         aliases = ['Name', 'Zone Type', query[-1].upper()+' Crime Frequency']
     if (query[1] == 'region' and query[0].lower() == 'all'):
-        data = gpd.read_file(path + '/zones/regions.geojson')
+        data = gpd.read_file(REGIONS_G)
         starting_zoom = 5
         fields = ['name', 'zone_type', 'sum']
         aliases = ['Name', 'Zone Type', query[-1].upper()+' Crime Frequency']
+    
+    if query[0] == 'all':
+        starting_zoom = 5
+
     data['name'] = data['name'].str.upper()
-    # for_plotting = results.merge(data, left_on = 'name', right_on = 'name')
-    merged2 = data.merge(results,on="name")
-    # merged = gpd.GeoDataFrame(for_plotting)
-    # geo_j = merged.to_json()
+    # merging the geodata and aggregated data
+    merged = data.merge(results,on="name")
 
     # finding coordinates of starting location
     if query[0].lower() != 'all':
@@ -243,6 +262,7 @@ def choropleth(query):
         # starting location coordinates [lat, long] set to Perth if no coordinates found
             starting_point = [-32, 116]
     else:
+        # starting location coordinates [lat, long] set to centre of WA if 'all' query
         starting_point = [-25.32805556, 122.29833333]
     
     # creating a map object for choropleth map
@@ -254,36 +274,36 @@ def choropleth(query):
         min_zoom=3,
         attr='Crime Heatmap')
 
-    #Creating choropleth map object with key on suburb name
+    # creating choropleth map object with key on suburb name
     folium.Choropleth(
-        geo_data = merged2, #Assign geo_data to your geojson file
+        geo_data = merged, # assign geo_data to your geojson file
         name = "WA Crime Heat Map",
-        data = merged2, #Assign dataset of interest
-        columns = ['name', 'log'], #Assign columns in the dataset for plotting
-        key_on = 'feature.properties.name', #Assign the key that geojson uses to connect with dataset
+        data = merged, # assign dataset of interest
+        columns = ['name', 'log'], # assign columns in the dataset for plotting
+        key_on = 'feature.properties.name', # assign the key that geojson uses to connect with dataset
         fill_color = 'YlOrRd',
         fill_opacity = 0.4,
         line_opacity = 0.5,
         bins = 9,
         legend_name = 'legend').add_to(choropleth)
     
-    #Creating style_function
+    # creating style_function
     style_function = lambda x: {
                     'fillColor': '#ffffff', 
                     'color':'#000000', 
                     'fillOpacity': 0.1, 
                     'weight': 0.1}
 
-    #Creating highlight_function
+    # creating highlight_function
     highlight_function = lambda x: {
                     'fillColor': '#000000', 
                     'color':'#000000', 
                     'fillOpacity': 0.50, 
                     'weight': 0.1}
 
-    # Creating popup tooltip object
+    # creating popup tooltip object
     NIL = folium.features.GeoJson(
-        merged2,
+        merged,
         style_function=style_function, 
         control=False,
         highlight_function=highlight_function, 
@@ -292,11 +312,12 @@ def choropleth(query):
             aliases=aliases,
             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")))
 
-    #Adding tooltip object to the map
+    # adding tooltip object to the map
     choropleth.add_child(NIL)
     choropleth.keep_in_front(NIL)
     folium.LayerControl().add_to(choropleth)
     
+    # adding anomalies to the map
     if query[0].lower() != 'all':
         if isinstance(anomalies, pd.DataFrame):
             anomaly_locations = list(anomalies['name'].unique())
@@ -305,14 +326,11 @@ def choropleth(query):
             try:
                 for i in anomaly_locations:
                     if query[1] == 'suburb' or 'station':
-                        with open(r'{}'.format(COORDINATES)) as f:
-                            coordinates = json.load(f)
+                        coordinates = coordinates_suburbs
                     if query[1] == 'district':
-                        with open(r'{}'.format(STATIONS)) as f:
-                            coordinates = json.load(f)
+                        coordinates = coordinates_stations
                     if query[1] == 'region':
-                        with open(r'{}'.format(DISTRICTS)) as f:
-                            coordinates = json.load(f)
+                        coordinates = coordinates_districts
                     coordinates = dict(coordinates)
                     location = coordinates[i.lower()]
                     if location != '':
@@ -320,21 +338,20 @@ def choropleth(query):
                         html_df.index = np.arange(1, len(html_df) + 1)
                         html = html_df.to_html(classes=
                             "table table-striped table-hover table-condensed table-responsive")
-                    # iframe = folium.IFrame(html=html,
-                    #        width=100,
-                    #        height=100)
                         folium.Marker(
                             location=location, 
                             icon=folium.Icon(color="blue",icon="map-pin", prefix='fa'),
                             popup=folium.Popup(html),
                         ).add_to(choropleth)
             except:
-                print('hi')
+                print('anomaly error')
 
     choropleth.save('generated_map.html')
     return plot
     
 def main():
+    # many functions have been commented out as they are not required for the running of the app
+    # they were only required for the generation of the mapping data files
     for file in FILES:
         if check_file(file) == None:
             return None
@@ -343,7 +360,7 @@ def main():
     # missing_landgate = missing_locations(zones_data, zones, missing2)
     # changes = assign_missing_boundaries(missing_landgate, landgate_data, changes, crime_locations)
     # final_data = zoning_boundaries(changes, landgate_data, zones, crime_locations, landgate_locations, zones_data)
-    choropleth(["regional wa region", "region", "all", "all", "all"])
+    choropleth(query)
     # return final_data
 
 if __name__ == '__main__':
